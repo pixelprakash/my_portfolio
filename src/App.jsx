82956176fc1b4c928289309983709d0fca4360f2
Musunuri Naga Sai Sri Surya Prakash <hellomusunuri@gmail.com>
2026-01-13 13:55:56 +0530

    Initial Vite + React setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Surya Prakash Musunuri</h1>
+        <h2> UX/UI Designer and Developer</h2>
+        <h3>Working at DIC, Design Department, IIT Hyderabad</h3>
+
+       </div>
+    </>
+  )
+}
+
+export default App
